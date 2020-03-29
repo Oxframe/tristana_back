3d76b32eeaf2753d0a5d15b0ef6182af8faff8c4
616653486@qq.com <Qian.521>
2020-03-29 11:29:22 +0800

    测试

---
+++ ts_basic/src/main/java/ts/basic/TsBasicApplication.java
@@ -0,0 +1,13 @@
+package ts.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsBasicApplication.class, args);
+    }
+
+}
---
+++ ts_basic/src/test/java/ts/basic/TsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package ts.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
